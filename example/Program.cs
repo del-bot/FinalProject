6242ed69409b404c282383ff8f65e56c7f2a9028
Delyash <delyash91@mail.ru>
2023-03-09 20:03:50 -0500

    programm

---
+++ example/Program.cs
@@ -0,0 +1,27 @@
+﻿void FindSM (string[] FirstArray, string[] SecondArray)
+{
+    int k = 0;
+    for (int i = 0; i < FirstArray.Length; i++)
+    {
+        if(FirstArray[i].Length <= 3)
+        {
+            SecondArray[k] = FirstArray[i];
+            k++;
+        }
+    }
+}
+
+void PrintArray(string [] printArray)
+{
+    for (int i = 0; i < printArray.Length; i++)
+    {
+        Console.Write($"{printArray[i]}");
+    }
+    Console.WriteLine();
+}
+
+string [] UseFirstArray = new string [4]{"hello","2","world",":-)"};
+string [] SecArray = new string[UseFirstArray.Length];
+
+FindSM(UseFirstArray, SecArray);
+PrintArray(SecArray);
